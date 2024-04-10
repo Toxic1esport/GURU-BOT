7b378fe397c1f16aec401d1c6c3d1eea47996bb3
Toxic1esport <166655834+Toxic1esport@users.noreply.github.com>
2024-04-10 21:39:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['8167063337', 'TOXIC', true],
+  ['8167063337', 'Esports', true], 
   [''] 
 ] //Number of owners
 
